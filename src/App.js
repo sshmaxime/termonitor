354177fd90b55b69aef7833f223f92ae3be316e6
Maxime Aubanel <maximeauba@gmail.com>
2019-05-09 02:32:08 +1000

    :tada: React + Redux + Electron

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ToDO from "./pages/todo";
+import { Provider as ReduxProvider } from "react-redux";
+import configureStore from "./modules/store";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const reduxStore = configureStore(window.REDUX_INITIAL_DATA);
+
+class App extends Component {
+  render() {
+    return (
+      <ReduxProvider store={reduxStore}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">ToDo Redux app</h1>
+          </header>
+          <ToDO />
+        </div>
+      </ReduxProvider>
+    );
+  }
 }
 
 export default App;
+
+
+
+// import React from 'react';
+// import './App.css';
+// import Loading from './components/Loading'
+// import ToDO from './components/Dashboard'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <ToDO></ToDO>
+//     </div>
+//   );
+// }
+
+// export default App;
