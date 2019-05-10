0adc8a76404e1e627083ea72216ee800b460d91b
Maxime Aubanel <maximeauba@gmail.com>
2019-05-11 01:26:10 +1000

    :tada: Add Travis

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import { Provider as ReduxProvider } from "react-redux";
+
+import configureStore from "./modules/store";
+// Create Redux store
+const reduxStore = configureStore(window.REDUX_INITIAL_DATA);
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <ReduxProvider store={reduxStore}>
+      <App />
+    </ReduxProvider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
