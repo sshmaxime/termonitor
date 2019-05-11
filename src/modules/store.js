a79a1b202999ec35d1c65dba4558e2c9353d0311
Maxime Aubanel <maximeauba@gmail.com>
2019-05-12 06:54:42 +1000

    :sparkles: Add Thunk & Axios

--- src/modules/store.js
+++ src/modules/store.js
@@ -2,10 +2,14 @@ import { createStore, applyMiddleware } from "redux";
 
 // Logger with default options
 import logger from "redux-logger";
-
+import thunk from "redux-thunk";
 import reducer from "./reducer";
 
 export default function configureStore(initialState) {
-  const store = createStore(reducer, initialState, applyMiddleware(logger));
+  const store = createStore(
+    reducer,
+    initialState,
+    applyMiddleware(logger, thunk)
+  );
   return store;
 }
